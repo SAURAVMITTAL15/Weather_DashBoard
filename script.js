59fb7d3c09aa1e745b397e233a16bfb76016ffb9
Saurav Mittal <sauravagrawal2005@gmail.com>
2025-05-09 22:34:20 +0530

    create variables in html elements

---
+++ script.js
@@ -0,0 +1,19 @@
+const userLocation = document.getElementById('userLocation');
+const convertor = document.getElementById('convertor');
+const weatherIcon = document.querySelector('.weatherIcon');
+const temperature = document.querySelector('.temperature');
+const feelsLike = document.querySelector('.feelsLike');
+const description = document.querySelector('.description');
+const date = document.querySelector('.date');
+const city = document.querySelector('.city');
+
+
+const Hvalue = document.getElementById('Hvalue');
+const Wvalue = document.getElementById('Wvalue');
+const SRvalue = document.getElementById('SRvalue');
+const SSvalue = document.getElementById('SSvalue');
+const Cvalue = document.getElementById('Cvalue');
+const UVvalue = document.getElementById('UVvalue');
+const Pvalue = document.getElementById('Pvalue');
+
+const foreCast = document.querySelector('foreCast');
